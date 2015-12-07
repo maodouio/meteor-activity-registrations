1f0a51204460ea34ec2b83972aef34f9aebc3666
mycode <2894220@qq.com>
2015-12-07 15:55:29 +0800

    Update package version and delete file.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'maodouio:activity-registrations',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Maodou.io activity-registrations package.',
   // URL to the Git repository containing the source code for this package.
